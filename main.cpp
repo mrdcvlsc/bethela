f5ba3b28a16a9fd580dd06783fc94ee88a439525
mrdcvlsc <mordecaivelasco@gmail.com>
2021-12-21 20:18:27 +0800

    working for right input

--- main.cpp
+++ main.cpp
@@ -262,7 +262,7 @@ int main(int argc, char* args[])
 
                     bconst::byte* decrypt_raw;
                     #ifndef USE_CRYPTOPP
-                    std::pair<bconst::byte*,size_t> cipher = krypt.decrypt(filebytestream.data(),filebytestream.size());
+                    std::pair<bconst::byte*,size_t> cipher = krypt.decrypt(filebytestream.data(),output_len);
                     decrypt_raw = cipher.first;
                     output_len = cipher.second;
                     #else
